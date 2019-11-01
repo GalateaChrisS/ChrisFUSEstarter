87dae88bdef28b5eccf5eb753ac20ee01cfa79b2
Csimonetti <Christopher.Simonetti@galatea-assoicates.com>
2019-11-01 15:56:05 -0400

    cleanup

--- src/main/java/org/galatea/starter/service/StockPriceFormatter.java
+++ src/main/java/org/galatea/starter/service/StockPriceFormatter.java
@@ -23,7 +23,6 @@ public class StockPriceFormatter {
     ObjectMapper mapper = new ObjectMapper();
     mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-//    return mapper.writeValueAsString(stockPriceDataList);
     return stockPriceDataList.toString();
   }
 
